e1360fa48b622d77dc9721885d0160f186fac434
Christian Laurian Reyes <40213031+ChrisLaurian@users.noreply.github.com>
2023-01-17 08:12:49 -0600

    Backend completa

--- Backend/app.js
+++ Backend/app.js
@@ -16,6 +16,13 @@ app.use(express.urlencoded({extended:false}));
 app.use(express.json()); 
 
 //CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 //Añadir prefijos a rutas /cargar ruta
 app.use('/', article_router);
--- Backend/controllers/article.js
+++ Backend/controllers/article.js
@@ -294,7 +294,43 @@ var controller = {
                 mesagge: 'ninguna image con este nombre'
             });
         }
-    } 
+    },
+
+    //Metodo buscar
+    search: (req, res) =>{
+
+        //Sacar el string a buscar
+        var searchString = req.params.search;
+
+        //Find or
+        Article.find({"$or": [
+            { "title": { "$regex": searchString, "$options": "i"}},
+            { "content": { "$regex": searchString, "$options": "i"}}
+
+            ]})
+            .sort([['date', 'descending']])
+            .exec((err, articles) =>{
+
+                if(err){
+                    return res.status(500).send({
+                        status: 'error',
+                        mesagge: 'Error en la petición'
+                    });
+                }
+
+                if(!articles || articles.length <= 0){
+                    return res.status(404).send({
+                        status: 'error',
+                        mesagge: 'No hay articulos que coincidan con tu busqueda'
+                    });
+                }
+
+                return res.status(200).send({
+                    status: 'success',
+                    articles
+                });
+            });
+    }
 
 }; //end controllador
 
